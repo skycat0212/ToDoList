7cbd21ae3602bb7de5ff94ad43842fccda2ffc1b
skycat0212 <mrmagnet777@gmail.com>
2023-03-11 15:06:19 +0900

    임시값 확인용 TODO 주석 작성

--- ToDoListUI/Sources/ToDoListUI/TLTodoListViewController.swift
+++ ToDoListUI/Sources/ToDoListUI/TLTodoListViewController.swift
@@ -12,6 +12,7 @@ public class TLTodoListViewController: UIViewController {
     struct TodoList {}
     
     // MARK: - property
+    // TODO: 초기값 없애기
     var todoList: [TodoList] = [.init(), .init(), .init()] {
         didSet {
             self.tableView.reloadData()
