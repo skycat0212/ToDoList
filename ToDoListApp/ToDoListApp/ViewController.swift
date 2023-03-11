6da52b4983e14fee3006f2f26bf5f546a3457ce2
skycat0212 <mrmagnet777@gmail.com>
2023-03-11 14:18:43 +0900

    xcode 프로젝트 생성

---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by 김수진 on 2023/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
