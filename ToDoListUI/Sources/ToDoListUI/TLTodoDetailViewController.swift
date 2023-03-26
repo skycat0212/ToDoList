050ee9d409080a7cebe79f85b663528f285a1e60
skycat0212 <mrmagnet777@gmail.com>
2023-03-26 19:13:36 +0900

    투두 상세보기 뷰컨 레이아웃 설정

--- ToDoListUI/Sources/ToDoListUI/TLTodoDetailViewController.swift
+++ ToDoListUI/Sources/ToDoListUI/TLTodoDetailViewController.swift
@@ -11,11 +11,30 @@ import TLCore
 
 public class TLTodoDetailViewController: UIViewController {
     
-    public let todo: TLTodo
+    public var todo: TLTodo {
+        didSet {
+            self.titleTextView.text = todo.title
+            self.descriptionTextView.text = todo.description
+        }
+    }
+    
+    public var safeArea: UILayoutGuide { self.view.safeAreaLayoutGuide }
+    private let titleTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        return textView
+    }()
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        return textView
+    }()
     
     public init(todo: TLTodo) {
         self.todo = todo
         super.init(nibName: nil, bundle: nil)
+        self.titleTextView.text = todo.title
+        self.descriptionTextView.text = todo.description
         
     }
     
@@ -26,7 +45,51 @@ public class TLTodoDetailViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setupViews()
+    }
+    
+    private func setupViews() {
+        let titleLabel: UILabel = {
+            $0.text = "Todo"
+            return $0
+        }(UILabel())
+        let descriptionLabel: UILabel = {
+            $0.text = "Description"
+            return $0
+        }(UILabel())
+        
+        let stackView: UIStackView = {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.axis = .vertical
+            $0.distribution = .fill
+            $0.backgroundColor = .yellow
+            return $0
+        }(UIStackView(
+            arrangedSubviews: [
+                titleLabel,
+                titleTextView,
+                descriptionLabel,
+                descriptionTextView
+            ]
+        ))
+        
+        titleLabel.heightAnchor.constraint(equalTo: descriptionLabel.heightAnchor).isActive = true
+        titleTextView.heightAnchor.constraint(equalTo: descriptionTextView.heightAnchor).isActive = true
+        titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        descriptionLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        
+        titleTextView.setContentHuggingPriority(.defaultLow, for: .vertical)
+        descriptionTextView.setContentHuggingPriority(.defaultLow, for: .vertical)
+        
+        self.view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -20),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+        ])
+        
+        
     }
 
 }
